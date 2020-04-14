c493d71a19e823cdb78a469fd954ac366809893d
gwenzler <gwenzler@ucsc.edu>
2020-04-14 00:33:33 -0700

    working spaceships

---
+++ prefabs/Spaceship.js
@@ -0,0 +1,17 @@
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this); // add to existing scene, displayList, updateList
+        //store pointValue
+        this.points = pointValue;
+    }
+
+    update() {
+        //move spaceship left
+        this.x -= 3;
+        // wraparound from left to right edge
+        if (this.x <= 0-this.width) {
+            this.x = game.config.width;
+        }
+    }
+}
