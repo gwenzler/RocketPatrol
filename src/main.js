f3396b1d628b1e50272c4620a615bb61f7065f01
gwenzler <gwenzler@ucsc.edu>
2020-04-14 02:18:53 -0700

    Menu & difficulty functional

--- src/main.js
+++ src/main.js
@@ -6,5 +6,9 @@ let config = {
 }
 let game = new Phaser.Game(config);
 
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000
+}
 //reserve keyboard vars
 let keyF, keyLEFT, keyRIGHT;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -65,9 +65,9 @@ class Play extends Phaser.Scene {
         
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(3000, () => {
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, '(F)ire to Restart',scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, '(F)ire to Restart or ← for Menu',scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
     }
@@ -76,6 +76,10 @@ class Play extends Phaser.Scene {
         //check key input for restart
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyF)){
             this.scene.restart(this.p1Score);
+        }
+
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            this.scene.start("menuScene");
         }
 
         this.starfield.tilePositionX -= 4;
