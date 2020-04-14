2109ae3594bb419bc7f9f1f3bbe23f300da9b4b7
gwenzler <gwenzler@ucsc.edu>
2020-04-14 00:22:49 -0700

    rocket.js basic functionality

--- prefabs/Rocket.js
+++ prefabs/Rocket.js
@@ -4,5 +4,30 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         //add object to existing scene
         scene.add.existing(this);
+        this.isFiring = false;
+    }
+
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >=47){
+                this.x -= 2;
+            }else if (keyRIGHT.isDown && this.x <= 578) {
+                this.x += 2;
+            }
+        }
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF)){
+            this.isFiring = true;
+        }
+        // if fired, move up
+        if(this.isFiring && this.y >=108) {
+            this.y -= 2;
+        }
+        // reset on miss
+        if(this.y <= 108) {
+            this.isFiring =false;
+            this.y = 431;
+        }
     }
 }
